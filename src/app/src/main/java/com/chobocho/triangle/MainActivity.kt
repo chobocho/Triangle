5a1c899a89e91c88c26e34c8a4b8c2a6c9c9e669
=Seunghwa Cho <chobocho99@gmail.com>
2018-11-14 00:43:26 +0900

    Add area, round

--- src/app/src/main/java/com/chobocho/triangle/MainActivity.kt
+++ src/app/src/main/java/com/chobocho/triangle/MainActivity.kt
@@ -26,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         val cornerA = findViewById<EditText>(R.id.triCornerA)
         val cornerB = findViewById<EditText>(R.id.triCornerB)
         val cornerC = findViewById<TextView>(R.id.triCornerC)
+        val angleA = findViewById<TextView>(R.id.triAngleA)
+        val angleB = findViewById<TextView>(R.id.triAngleB)
+        val area = findViewById<TextView>(R.id.area)
+
         val btnClear = findViewById<Button>(R.id.clear)
         val stringUtil = StringUtil()
 
@@ -34,26 +38,50 @@ class MainActivity : AppCompatActivity() {
             cornerB.setText("")
             cornerC.setText("")
             cornerC.setHint("C")
+            angleA.setText("")
+            angleB.setText("")
         }
 
         val btnAnswer = findViewById<Button>(R.id.answer)
 
         btnAnswer.setOnClickListener {
-               val triA = cornerA.getText()
-               val triB = cornerB.getText()
+            val triA = cornerA.getText()
+            val triB = cornerB.getText()
 
-               if (TextUtils.isEmpty(triA) || TextUtils.isEmpty(triB) ) {
-                   Toast.makeText(this, R.string.input_a_and_b, Toast.LENGTH_SHORT).show()
-                   return@setOnClickListener
-               }
-               Log.d(LOG_TAG, "Tri A: " + triA + " Tri B: " + triB)
+            if (TextUtils.isEmpty(triA) || TextUtils.isEmpty(triB) ) {
+                Toast.makeText(this, R.string.input_a_and_b, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
-               val nTriA = triA.toString().toDouble()
-               val nTriB = triB.toString().toDouble()
-               val triC = Math.sqrt( nTriA * nTriA + nTriB*nTriB)
-               val strTriC =  stringUtil.removeZero(String.format("%.3f", triC))
+            val nTriA = triA.toString().toDouble()
+            val nTriB = triB.toString().toDouble()
 
-               cornerC.setText(strTriC)
+            if(nTriA <= 0 || nTriB <= 0) {
+                Toast.makeText(this, R.string.input_a_and_b, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Log.d(LOG_TAG, "Tri A: " + triA + " Tri B: " + triB)
+
+            val nTriC = Math.sqrt( nTriA * nTriA + nTriB*nTriB)
+            val strTriC =  stringUtil.removeZero(String.format("%.3f", nTriC))
+
+            cornerC.setText(strTriC)
+
+            val nAngleA = Math.toDegrees(Math.atan(nTriB /nTriA))
+            val strAngleA = stringUtil.removeZero(String.format("%.3f", nAngleA))
+            angleA.setText(strAngleA)
+
+            val nAngleB = 90 - nAngleA
+            val strAngleB = stringUtil.removeZero(String.format("%.3f", nAngleB))
+            angleB.setText(strAngleB)
+
+            val nRound = nTriA + nTriB + nTriC
+            val strRound = stringUtil.removeZero(String.format("%.3f", nRound))
+            val nArea = (nTriA * nTriB)/2
+            val strArea = stringUtil.removeZero(String.format("%.3f", nArea))
+            val strResult = getText(R.string.round).toString() + strRound + "\n" + getText(R.string.area).toString() + strArea
+            area.setText(strResult)
         }
     }
 }
